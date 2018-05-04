d0ddda8018e06d55d2e315e24c33aa94264275d9
GuillaumeCreusot <gcreusot@ensc.fr>
2018-05-04 10:48:30 +0200

    Widget

---
+++ RushHour/RushHour/View/Widget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour.View
+{
+    class Widget
+    {
+        private Widget master;
+        private string name;
+
+        public Widget Master { get => master; private set => master = value; }
+        public string Name { get => name; private set => name = value; }
+
+        public string Content
+        {
+            get
+            {
+                return "Content";
+            }
+        }
+    }
+}
