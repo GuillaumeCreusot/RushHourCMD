364be286d4d94e25cb8b05dc5ac6824f3a436070
Louis Hache <36883441+LouisHache@users.noreply.github.com>
2018-05-09 12:32:20 +0200

    MGrid

--- RushHour/RushHour/Model/MGrid.cs
+++ RushHour/RushHour/Model/MGrid.cs
@@ -17,10 +17,48 @@ namespace RushHour.Model
 
         public bool ValidDirection(MVehicle vehicle, MMain.Direction direction)
         {
-            return true;
+            int x = vehicle.Pos[0];
+            int y = vehicle.Pos[1];
+            if (vehicle.VehicleDirection == direction || (int)vehicle.VehicleDirection == ((int)direction + 2) % 4)
+            {
+                if (direction == MMain.Direction.North) //North
+                    if (y - 1 >= 0)
+                        return !this[x, y - 1];
+
+                if (direction == MMain.Direction.West) //West
+                    if (x - 1 >= 0)
+                        return !this[x - 1, y ];
+
+                if (direction == MMain.Direction.South) //South
+                    if (y + 1 <= YLength)
+                        return !this[x, y + 1];
+
+                if (direction == MMain.Direction.East) //East
+                    if (x + 1 <= XLength)
+                        return !this[x + 1, y];
+            }
+
+            return false;
+        }
+
+        //Builder
+        public MGrid(int X, int Y)
+        {
+            XLength = X;
+            YLength = Y;
+            gridCollision = new bool[XLength, YLength];
+            Vehicles = new List<MVehicle>();
         }
 
 
+        //Index
+        public bool this[int x, int y]
+        {
+            get
+            {
+                return gridCollision[x, y];
+            }
+        }
 
 
     }
