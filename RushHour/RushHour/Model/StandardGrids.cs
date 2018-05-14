74e611a1c5b456a5a8f002736164c8ca41eaa8d9
AlexandraP <apometko@ensc.fr>
2018-05-14 16:13:33 +0200

    Standard Grids

--- RushHour/RushHour/Model/StandardGrids.cs
+++ RushHour/RushHour/Model/StandardGrids.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RushHour.Model
+namespace RushHour
 {
     static class StandardGrids
     {
-       public static int[] easyGrid = { };
+       //length, starting position X, position Y column, direction
+       public static int[] easyGrid = { { 3, 5, 4, MMain.Direction.North } };
+       public static int[] easyGrid2 = { { 3, 5, 2, MMain.Direction.North }, { 3, 5, 5, MMain.Direction.West }, { 2, 3, 4, MMain.Direction.South } };
+
+       public static int[] mediumGrid = { { 3, 1, 0, MMain.Direction.South }, { 3, 5, 0, MMain.Direction.East }, { 2, 1, 2, MMain.Direction.South }, { 2, 0, 3, MMain.Direction.East }, { 2, 1, 4, MMain.Direction.West }, { 3, 4, 3, MMain.Direction.North }, { 3, 2, 5, MMain.Direction.South } };
+
+       public static int[] hardGrid = { { 2, 5, 0, MMain.Direction.North }, { 2, 3, 0, MMain.Direction.East }, { 2, 3, 3, MMain.Direction.West }, { 2, 1, 2, MMain.Direction.East }, { 3, 0, 5, MMain.Direction.West }, { 2, 1, 5, MMain.Direction.West }, { 2, 5, 3, MMain.Direction.North }, { 2, 5, 5, MMain.Direction.West }, { 3, 4, 4, MMain.Direction.North }, { 3, 2, 5, MMain.Direction.South } };
     }
 }
