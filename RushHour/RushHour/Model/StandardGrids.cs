1fc1aabf6cc8ff08281698d9e3faffbaa0346c69
AlexandraP <apometko@ensc.fr>
2018-05-14 14:32:00 +0200

    standart grids class

---
+++ RushHour/RushHour/Model/StandardGrids.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour.Model
+{
+    static class StandardGrids
+    {
+       public static int[] easyGrid = { };
+    }
+}
