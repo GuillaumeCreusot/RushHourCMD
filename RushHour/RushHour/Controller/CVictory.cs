b538c86ab94dd1130690853fdd8fab2b6e8d1350
alexandrapometko <apometko@ensc.fr>
2018-05-22 20:40:13 +0200

    Victory Screen and Re: Shower

---
+++ RushHour/RushHour/Controller/CVictory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour
+{
+    class CVictory
+    {
+        public VVictory menu;
+        public WidgetsManager manager;
+
+        public CVictory()
+        {
+            menu = new VVictory();
+            Label permanentText = new Label("permanent text", VVictory.text, VVictory.dimText[0] + 1, VVictory.dimText[1]);
+            manager = new WidgetsManager("Victory Menu", Console.LargestWindowWidth, Console.LargestWindowHeight);
+            manager.AddWidget(permanentText, 0, (Console.LargestWindowWidth / 2) - (VVictory.dimText[1] / 2));
+            manager.AddWidget(menu, VVictory.dimText[0] + 2, (Console.LargestWindowWidth / 2) - (VVictory.dimText[1] / 2));
+        }
+
+        public int Control()
+        {
+            manager.RefreshContentOnScreen();
+            while (true)
+            {
+                ConsoleKeyInfo k = Console.ReadKey();
+                switch (k.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (menu.SelectedItem == 0)
+                        {
+                            menu.SelectedItem = menu.NbItem - 1;
+                        }
+                        else
+                        {
+                            menu.SelectedItem--;
+                        }
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (menu.SelectedItem == menu.NbItem - 1)
+                        {
+                            menu.SelectedItem = 0;
+                        }
+                        else
+                        {
+                            menu.SelectedItem++;
+                        }
+                        break;
+
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+                        return menu.SelectedItem;
+                }
+            }
+        }
+    }
+}
---
+++ RushHour/RushHour/View/Widget/VVictory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushHour
+{
+    class VVictory : Menu
+    {
+
+        public static string text = @"
+                    ██╗     ██╗██╗  ██╗███████╗     █████╗     ██████╗  ██████╗ ███████╗███████╗██╗
+                    ██║     ██║██║ ██╔╝██╔════╝    ██╔══██╗    ██╔══██╗██╔═══██╗██╔════╝██╔════╝██║
+                    ██║     ██║█████╔╝ █████╗      ███████║    ██████╔╝██║   ██║███████╗███████╗██║
+                    ██║     ██║██╔═██╗ ██╔══╝      ██╔══██║    ██╔══██╗██║   ██║╚════██║╚════██║╚═╝
+                    ███████╗██║██║  ██╗███████╗    ██║  ██║    ██████╔╝╚██████╔╝███████║███████║██╗
+                    ╚══════╝╚═╝╚═╝  ╚═╝╚══════╝    ╚═╝  ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝╚═╝
+                                                                               
+
+                                    _________________________________________________
+                            /|     |                                                 |
+                            ||     |          On a rarement vu quelqu'un             |
+                        .----|-----,|         sortir de son créneau aussi bien.       |
+                        ||  ||   ==||                  Félicitations !                |
+                    .-----'--'|   ==||                                                 |
+                    |)-      ~|     ||_________________________________________________|
+                    | ___     |     |____...==..._  >\______________________________|
+                    [_/.-.\'--'-------- //.-.  .-.\\/   |/            \\ .-.  .-. //
+                    (o)`=== '''''''''`( o )( o )     o              `( o )( o )` 
+                    '-'                '-'  '-'                       '-'  '-'
+                                                                                                      
+                                    
+            ";
+        public static int[] dimText = WidgetUtility.DimContent(text);
+        public static string[] itemName = { "Nouvelle partie", "Quitter" };
+
+        public VVictory():base("Escape Menu", "", itemName.Length + 1, 30, itemName, margeLeft : 10)
+        {
+
+        }
+    }
+}
